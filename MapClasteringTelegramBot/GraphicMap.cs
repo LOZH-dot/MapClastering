2bbbf2e452b335328c97e4f8728520a5be46d4eb
Salieri <maxim.baschilov@yandex.ru>
2025-09-04 19:35:14 +0300

    Вывод изображения одного из кластеров в графическом виде (Yandex Static Map API)

---
+++ MapClasteringTelegramBot/GraphicMap.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapClasteringTelegramBot
+{
+    internal class GraphicMap
+    {
+        public static string GenerateStaticMapUrl(List<Coordinates> points)
+        {
+            // Используем сервис Static Map API
+            // Для этого примера возьмем [Static Map API от Yandex](https://tech.yandex.ru/maps/doc/geosearch/concepts/static-docpage/)
+            // Их формат URL:
+            // https://static-maps.yandex.ru/1.x/?ll=37.6173,55.7558&size=450,450&z=10&l=map&pt=37.6173,55.7558,pm2rdm|37.6491,55.6924,pm2rdm
+
+            string baseUrl = "https://static-maps.yandex.ru/1.x/";
+            string size = "600,400"; // размеры изображения
+            int zoom = 11; // уровень масштабирования
+
+            // Вычисляем центр карты
+            double avgLat = 0;
+            double avgLon = 0;
+            foreach (var p in points)
+            {
+                avgLat += p.Latitude;
+                avgLon += p.Longitude;
+            }
+            avgLat /= points.Count;
+            avgLon /= points.Count;
+
+            // Формируем строку точек
+            List<string> ptParams = new List<string>();
+            foreach (var p in points)
+            {
+                // 'pm2rdm' - маркер с красным лицом, можно выбрать другой стиль
+                ptParams.Add($"{p.Longitude.ToString().Replace(',', '.')},{p.Latitude.ToString().Replace(',', '.')},pm2rdm");
+            }
+            string ptString = string.Join("|", ptParams);
+
+            string url = $"{baseUrl}?ll={avgLon.ToString().Replace(',', '.')},{avgLat.ToString().Replace(',', '.')}&size={size}&z={zoom}&l=map&pt={ptString}";
+            return url;
+        }
+
+        public static async Task<byte[]> DownloadImageAsync(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                return await client.GetByteArrayAsync(url);
+            }
+        }
+    }
+}
--- MapClasteringTelegramBot/Program.cs
+++ MapClasteringTelegramBot/Program.cs
@@ -44,15 +44,15 @@ namespace MapClasteringTelegramBot
             // Входной массив адресов
             Application[] applications = new Application[]
             {
-                new Application { Address = "Владимир, Лакина, 2", Type = ApplicationType.G_PON },
-                new Application { Address = "Владимир, Лакина, 2А", Type = ApplicationType.G_PON },
+                new Application { Address = "Владимир, Лакина, 2", Type = ApplicationType.G_PON, DateTime = DateTime.Now },
+                new Application { Address = "Владимир, Лакина, 2А", Type = ApplicationType.G_PON, DateTime = DateTime.Now.AddHours(2) },
 
-                new Application { Address = "Владимир, Тракторная, 58", Type = ApplicationType.PACKET },
-                new Application { Address = "Владимир, Тракторная, 1", Type = ApplicationType.G_PON },
-                new Application { Address = "Владимир, Промышленный проезд, 5", Type = ApplicationType.PACKET },
+                new Application { Address = "Владимир, Тракторная, 58", Type = ApplicationType.PACKET, DateTime = DateTime.Now.AddHours(3) },
+                new Application { Address = "Владимир, Тракторная, 1", Type = ApplicationType.G_PON, DateTime = DateTime.Now.AddHours(4)},
+                new Application { Address = "Владимир, Промышленный проезд, 5", Type = ApplicationType.PACKET, DateTime = DateTime.Now.AddHours(5) },
 
-                new Application { Address = "Владимир, Верхняя Дуброва, 6", Type = ApplicationType.G_PON },
-                new Application { Address = "Владимир, Верхняя Дуброва 22", Type = ApplicationType.PACKET },
+                new Application { Address = "Владимир, Верхняя Дуброва, 6", Type = ApplicationType.G_PON, DateTime = DateTime.Now.AddHours(6)},
+                new Application { Address = "Владимир, Верхняя Дуброва 22", Type = ApplicationType.PACKET, DateTime = DateTime.Now.AddHours(1)},
             };
 
             // Геокодируем адреса
@@ -82,12 +82,31 @@ namespace MapClasteringTelegramBot
                 result += ($"Кластер {i + 1}:\n");
                 foreach (var coord in clusters[i])
                 {
-                    result += ($"  {coord.Application.Address} ({coord.Latitude}, {coord.Longitude}) - {coord.Application.Type.ToString()}\n");
+                    result += ($"  {coord.Application.Address} ({coord.Latitude}, {coord.Longitude}) [{coord.Application.DateTime.ToString("HH:mm")}] - {coord.Application.Type.ToString()}\n");
                 }
                 result += '\n';
             }
 
             await client.SendMessage(message.Chat, result);
+
+            // Построение URL для Static Map
+            List<Coordinates> AllPoints = new List<Coordinates>();
+
+            foreach (var item in clusters)
+                foreach (var point in item)
+                    AllPoints.Add(point);
+
+
+            string mapUrl = GraphicMap.GenerateStaticMapUrl(AllPoints);
+
+            // Загружаем изображение карты
+            byte[] imageBytes = await GraphicMap.DownloadImageAsync(mapUrl);
+
+            await client.SendPhoto(
+                chatId: message.Chat,
+                photo: new System.IO.MemoryStream(imageBytes),
+                caption: "Карта с точками для кластера"
+        );
         }
 
         static async Task<Coordinates> GeocodeAddress(string address)
