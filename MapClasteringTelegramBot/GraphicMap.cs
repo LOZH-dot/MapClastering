8daa2a64f7864be23a7b821af171126260ef23be
Salieri <maxim.baschilov@yandex.ru>
2025-09-05 21:04:33 +0300

    Убран код, связанный с ручным определением зума для графического отображения карты

--- MapClasteringTelegramBot/GraphicMap.cs
+++ MapClasteringTelegramBot/GraphicMap.cs
@@ -45,59 +45,5 @@ namespace MapClasteringTelegramBot
                 return await client.GetByteArrayAsync(url);
             }
         }
-
-        static int CalculateZoomLevel(List<Coordinates> points, double minLat, double maxLat, double minLon, double maxLon)
-        {
-            // Определим глобальные границы
-            double deltaLat = maxLat - minLat;
-            double deltaLon = maxLon - minLon;
-
-            // Размеры карты в пикселях
-            int width = 600;
-            int height = 400;
-
-            // Минимальный масштаб
-            int minZoom = 0;
-            int maxZoom = 17; // максимально допустимый уровень zoom для Yandex Static Maps
-
-            for (int zoom = maxZoom; zoom >= minZoom; zoom--)
-            {
-                // Расчет высоты и ширины области на данном zoom
-                double scale = Math.Pow(2, zoom);
-
-                // Высота и ширина области в метках (метка = 1/2^zoom границ в терминах карт)
-                // Математика основана на том, что:
-
-                // Функция для получения по ширине и высоте текущего zoom
-                double mapHeightInMeters = GetMapHeightInMeters(zoom);
-                double mapWidthInMeters = GetMapWidthInMeters(zoom);
-
-                // Преобразуем в пиксели
-                double latDegPerPixel = 360.0 / (256 * scale);
-                double lonDegPerPixel = 360.0 / (256 * scale);
-
-                // Расстояние в градусах по широте и долготе
-                double latDeltaInPixels = deltaLat / latDegPerPixel;
-                double lonDeltaInPixels = deltaLon / lonDegPerPixel;
-
-                // Проверяем, помещаются ли все точки в текущий zoom
-                if (latDeltaInPixels <= width && lonDeltaInPixels <= height)
-                {
-                    return zoom;
-                }
-            }
-
-            return minZoom; // Если не помещаются, берем минимальный zoom
-        }
-
-        static double GetMapHeightInMeters(int zoom)
-        {
-            return 20037508.34 * 2;
-        }
-
-        static double GetMapWidthInMeters(int zoom)
-        {
-            return GetMapHeightInMeters(zoom);
-        }
     }
 }
